6d9891498675ac44a74f1e5e487280a1254d1bdc
MOONTAESU <37867077+MOONTAESU@users.noreply.github.com>
2019-04-23 22:14:23 +0900

    Linking To Operator.java

--- triangle.java
+++ triangle.java
@@ -1,22 +1,30 @@
-package forsubject;
-import java.util.Scanner;
+
+//import java.util.Scanner;
 public class triangle {
-static Scanner scan=new Scanner(System.in);
 
-    public static void main(String[] args) {
-    System.out.print("삼각형 크기 : ");
-    int num = scan.nextInt();
-    String str = "*";
+	private int layer; // 변의길이
+	private String shape; // 모양
 
-    for(int i = 0; i < num; i++) {
-         for(int j = 0; j < num- (i + 1); j++) {
-              System.out.print(" ");
-     	}
-         for(int j=0; j< 1 + 2 * i; j++) {
-              System.out.print(str);
-        }
+  public triangle(int layer, String shape){ // 변의길의와 모양을 입력받아 필드값으로 저장
+	     this.layer = layer;
+	     this.shape = shape;
+}
 
-     	 System.out.println();
-     }
-    }
- }
+  public void Triangle_f() {
+
+    for(int i = 0; i < layer; i++) {
+          for(int j = 0; j < layer- (i + 1); j++) {
+              System.out.print("   ");
+          }
+          for(int j=0; j< 1 + 2 * i; j++) {
+              System.out.print(shape);
+            }
+            if(i==layer) {
+              break;
+            }
+            else {
+              System.out.println("");
+            }
+   }
+  }
+}
