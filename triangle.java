a744892152537ac14615ac71d6749d5ad90ac16b
MOONTAESU <37867077+MOONTAESU@users.noreply.github.com>
2019-04-23 16:16:32 +0900

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,25 +1,21 @@
 package forsubject; 
 import java.util.Scanner; 
-public class test {
+public class triangle {
 static Scanner scan=new Scanner(System.in);
 
-    public static void Inverted() {
-    System.out.print("삼각형 크기 : ");
-				int num = scan.nextInt();
+    public static void main(String[] args) {
+    System.out.print("삼각형 크기 : ");
+    int num = scan.nextInt();
     String str = "*";
 
-			    	for(int i = 0; i < num; i++) {
-      	     	for(int j = 0; j < num- (i + 1); j++) {
-                   System.out.print(" ");
-     			     }
-             for(int j=0; j<= 1 + 2 * i; j++) {
-                   System.out.print(str);
-             }
-     			     System.out.println();
-      		}
-     }
-       	public static void main(String[] args) {
-        Inverted();
-     	}
-
+    for(int i = 0; i < num; i++) {
+         for(int j = 0; j < num- (i + 1); j++) {
+              System.out.print(" ");
+     	}
+         for(int j=0; j<= 1 + 2 * i; j++) {
+              System.out.print(str);
+        }
+     	 System.out.println();
+     }
+    }
  }
