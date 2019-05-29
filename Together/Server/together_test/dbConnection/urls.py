7b91af9fa857d7fadf2dcf40091050770353c930
mickeyshoes <parody9c@gmail.com>
2019-05-30 04:14:39 +0900

    server to database

--- Together/Server/together_test/dbConnection/urls.py
+++ Together/Server/together_test/dbConnection/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('dbtest', views.dbtest),
     path('idtest', views.login),
     path('dictest', views.test_dictonary_ver),
+    path('reqtest', views.request_test),
+    path('app_login', views.app_login),
+    path('find_id', views.find_id),
+    path('find_pw', views.find_pw),
+    path('add_user', views.add_user),
 ]
--- Together/Server/together_test/dbConnection/views.py
+++ Together/Server/together_test/dbConnection/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 import pymysql
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 
 # Create your views here.
 
@@ -46,6 +50,111 @@ def test_dictonary_ver(request):
     print_dic = datas
     con.close()
     return HttpResponse(print_dic)
+
+def request_test(request):
     
+    request_int = request.POST.get('request_int', '')
+    request_int = request_int+ request_int
+    return HttpResponse(request_int)
+
+def app_login(request):
+    userID = "milkis"
+    UserPW = "12354"
+    try:
+        con = pymysql.connect(host='localhost', user='gohomie', password='qwerty', db='together_database', charset='utf8')
+        curs = con.cursor()
+        sql = 'select UPw from userinfo where UID =%s'
+        curs.execute(sql,userID)
+
+        datas = curs.fetchone()
+        print_datas= datas
+        print(print_datas[0])
+
+    finally: 
+        con.close()
+
+    if(UserPW == print_datas[0]):
+        print('login success')
+        return HttpResponse(1)
+    
+    else:
+        print('login fail')
+        return HttpResponse(0)
+
+def find_id(request):
+    userName = '롯데'
+    userTel = '080-730-1472'
+    try:
+        con = pymysql.connect(host='localhost', user='gohomie', password='qwerty', db='together_database', charset='utf8')
+        curs = con.cursor()
+        sql = 'select UID from userinfo where UName =%s and UTel =%s'
+        curs.execute(sql,(userName, userTel))
+
+        datas = curs.fetchone()
+        print_datas = datas
+
+    finally: 
+        con.close()
+
+    if(print_datas == None):
+        print('cannot find id')
+        return HttpResponse(0)
+    else:
+        return HttpResponse(print_datas[0])
+
+def find_pw(request):
+    userid = "milkis"
+    usertel = "080-730-1472"
+    try:
+        con = pymysql.connect(host='localhost', user='gohomie', password='qwerty', db='together_database', charset='utf8')
+        curs = con.cursor()
+        sql = 'select UPw from userinfo where UID =%s and UTel =%s'
+        curs.execute(sql,(userid, usertel))
+
+        datas = curs.fetchone()
+        print_datas = datas
+    finally: 
+        con.close()
+
+    if(print_datas == None):
+        print('cannot find pw')
+        return HttpResponse(0)
+    else:
+        return HttpResponse(print_datas[0])
+
+def add_user(request):
+    UID ='scrawbars'
+    UPw ='0948752'
+    UName = '박죠스'
+    UTel ='010-7788-0094'
+    errornum = 1 #error가 생기면 값을 0으로 바꾸고 0을 리턴
+
+    try:
+        con = pymysql.connect(host='localhost', user='gohomie', password='qwerty', db='together_database', charset='utf8')
+        curs = con.cursor()
+        sql = 'insert into userinfo values (%s, %s, %s, %s)'
+        curs.execute(sql,(UID, UPw, UName, UTel))
+        con.commit()
+
+    except con.Error as error :
+        con.rollback()
+        print('error inserting record into mysql')
+        errornum = 0
+        
+    finally: 
+        con.close()
+
+    if(errornum !=1):
+        return HttpResponse(0)
+    else:
+        return HttpResponse(1)
+
+#def change_pw(request):
+
+    
+
+
+
+
 
 
